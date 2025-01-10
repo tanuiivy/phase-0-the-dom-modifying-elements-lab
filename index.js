1a3674de17f65c25bf55bfcee379779211b7e63f
Ivy Tanui <ivy.tanui@strathmore.edu>
2025-01-10 13:21:18 +0300

    Commit

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const main = document.getElementById('main');
+main.remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+
+newHeader.innerHTML = 'Ivy Tanui is the champion';
+
+document.body.append(newHeader);
